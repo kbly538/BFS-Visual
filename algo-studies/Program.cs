0b5f4578a01aa1bd435aaa0193d37cae7a0e4d7f
kbly538 <kubilay.bilgili@gmail.com>
2022-09-09 04:23:14 +0300

    comments and clean up

--- algo-studies/Program.cs
+++ algo-studies/Program.cs
@@ -2,7 +2,8 @@
 using System.Drawing;
 
 
-String rootPath = @"D:\kbly538\source\repos\algo-studies\algo-studies\Maps\";
+//String rootPath = @"..\source\repos\algo-studies\algo-studies\Maps\";
+String rootPath = "..\\..\\..\\Maps\\";
 String textFileName = "starMap.txt";
 String imageName = "hand-drawn-map.png";
 
@@ -10,25 +11,47 @@ String fullPthToText = rootPath + textFileName;
 String fullPathToImage = rootPath + imageName;
 
 
-
-
-/*
- * BFS with textMapLoader
- */
-//MapLoader mapLoader = new MapLoader(fullPthToText);
-//mapLoader.DrawMap();
-//BFS bfs = new BFS(mapLoader);
-//bfs.StartBFS((1, 1), (5, 60));
-
-
 /*
  * BFS with map from image
  */
-BitmapMapLoader bml = new BitmapMapLoader(fullPathToImage);
-bml.CreateMapFile(fullPthToText);
+BitmapMapLoader bml = new BitmapMapLoader(fullPathToImage); 
+
+//Creates a text file using the image
+// e.g. 
+//	  x 
+//	  1	####### 
+//	  2	#     # 
+//	  3	#   ### 
+//	  4	#     #
+//	  5	# #   # 
+//	  6	#     # 
+//	  7	#######
+//      1234567 y     
+
+bml.CreateMapFile(fullPthToText); 
 BFS bfs = new BFS(bml);
 
 
+
+
+// Given coords, finds the shortest path using bfs
+
+
+// e.g. bfs.StartBFS((2, 2), (6, 6)); 
+//
+// starting point(s) : (2, 2)
+// destination(d) : (6, 6)
+//	
+//    
+//	  x 
+//	  1	####### 
+//	  2	#s    # 
+//	  3	#   ### 
+//	  4	#     #
+//	  5	# #   # 
+//	  6	#    d# 
+//	  7	#######
+//      1234567 y    
 bfs.StartBFS((9, 21), (49,58));
 
 
